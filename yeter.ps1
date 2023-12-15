e77d54d1ec5fa8b7958d58371fd4fa688d0dc274
IntellectueleCapybara <81165007+IntellectueleCapybara@users.noreply.github.com>
2023-12-15 13:05:31 +0100

    testbestand aanmaken

---
+++ yeter.ps1
@@ -0,0 +1,32 @@
+# Replace the parameters with your own values
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$DomainName,
+    [Parameter(Mandatory=$true)]
+    [string]$AdministratorPassword
+)
+
+$PSDefaultParameterValues = @{"*:Force" = $true; "*:Confirm" = $false}
+
+Install-PackageProvider -Name NuGet -Force
+#installeren van microsoft entraID en andere benodigte packages
+Install-Module MicrosoftEntraID -Force
+
+# Import the Microsoft Entra ID module
+Import-Module MicrosoftEntraID
+
+# Connect to the Microsoft Entra tenant
+Connect-MicrosoftEntraID -TenantId $env:TenantId
+
+# Get the managed domain details
+$managedDomain = Get-MicrosoftEntraIDManagedDomain -Name $DomainName
+
+# Join the VM to the managed domain
+$joinResult = Add-MicrosoftEntraIDComputerToManagedDomain -ManagedDomain $managedDomain -AdministratorPassword $AdministratorPassword
+
+# Check the join result
+if ($joinResult.IsSuccess) {
+    Write-Output "Successfully joined the VM to the managed domain $DomainName"
+} else {
+    Write-Error "Failed to join the VM to the managed domain $DomainName. Error: $joinResult.Error"
+}
