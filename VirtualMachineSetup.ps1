111fa11ad249a5bf84b0cf735aef045ef97feca3
Luuk <38768606+luckyluuk28@users.noreply.github.com>
2023-12-15 11:19:52 +0100

    Update VirtualMachineSetup.ps1

--- VirtualMachineSetup.ps1
+++ VirtualMachineSetup.ps1
@@ -1,3 +1,10 @@
+param (
+  [Parameter(Mandatory=$true)]
+  [string]$DomainName,
+  [Parameter(Mandatory=$true)]
+  [string]$SafeModeAdministratorPassword
+)
+
 powershell -ExecutionPolicy bypass -File office-proplus-deployment.ps1 -OfficeVersion Office2016
 powershell -ExecutionPolicy bypass -File 7ZipSetup.ps1
-powershell -ExecutionPolicy bypass -File join-AD.ps1
+powershell -ExecutionPolicy bypass -File join-AD.ps1 $DomainName $AdministratorPassword
