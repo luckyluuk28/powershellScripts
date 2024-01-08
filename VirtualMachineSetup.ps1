c93858ad859e23e4e187889e59224b6c8613e6ee
Luuk <38768606+luckyluuk28@users.noreply.github.com>
2024-01-08 13:11:59 +0100

    Update VirtualMachineSetup.ps1

--- VirtualMachineSetup.ps1
+++ VirtualMachineSetup.ps1
@@ -1,10 +1,14 @@
 param (
   [Parameter(Mandatory=$true)]
+  [string]$IpAddress
+  [Parameter(Mandatory=$true)]
   [string]$DomainName,
   [Parameter(Mandatory=$true)]
-  [string]$AdministratorPassword
+  [string]$Username
+  [Parameter(Mandatory=$true)]
+  [string]$Password
 )
 
 powershell -ExecutionPolicy bypass -File office-proplus-deployment.ps1 -OfficeVersion Office2016
 powershell -ExecutionPolicy bypass -File 7ZipSetup.ps1
-powershell -ExecutionPolicy bypass -File join-AD.ps1 -IpAddress '10.11.11.11' -Username 'adminuser' -DomainName $DomainName -Password $AdministratorPassword
+powershell -ExecutionPolicy bypass -File join-AD.ps1 -IpAddress $IpAddress -Username $Username -DomainName $DomainName -Password $Password
