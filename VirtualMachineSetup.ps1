0cd88c5a94ed76914086bf2a5e6da12e79c191c7
Luuk <38768606+luckyluuk28@users.noreply.github.com>
2024-01-28 17:17:53 +0100

    Update VirtualMachineSetup.ps1

--- VirtualMachineSetup.ps1
+++ VirtualMachineSetup.ps1
@@ -11,16 +11,6 @@ param (
 
 Set-TimeZone -Id "W. Europe Standard Time"  
 
-if (-not $Password) {
-    try {
-        $Password = $env:ExtensionContext.GetProtectedSetting('adminPassword')
-    } catch {
-        Write-Error "Failed to retrieve adminPassword from protectedSettings."
-        # Handle the error or provide a default password here if needed
-        return
-    }
-}
-
 New-Item -ItemType Directory -Path "C:\Program Files\Splunk"
 Invoke-WebRequest -Uri "https://download.splunk.com/products/universalforwarder/releases/9.1.2/windows/splunkforwarder-9.1.2-b6b9c8185839-x64-release.msi" -OutFile "C:\Program Files\Splunk\splunkforwarder.msi"
 Start-Process msiexec.exe -Wait -ArgumentList '/i "C:\Program Files\Splunk\splunkforwarder.msi" DEPLOYMENT_SERVER="10.1.0.31:8089" RECEIVING_INDEXER="10.1.0.31:9997" LAUNCHSPLUNK=1 SERVICESTARTTYPE=auto WINEVENTLOG_APP_ENABLE=1 WINEVENTLOG_SEC_ENABLE=1 WINEVENTLOG_SYS_ENABLE=1 WINEVENTLOG_FWD_ENABLE=1 WINEVENTLOG_SET_ENABLE=1 ENABLEADMON=1 SPLUNKPASSWORD="Kaaseter!1" PRIVILEGEBACKUP=1 PRIVILEGESECURITY=1 AGREETOLICENSE=yes /quiet /qn"' 
