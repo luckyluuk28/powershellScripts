8821ef5e37d9054a1ba6351c7f88fd732578f0e4
Luuk <38768606+luckyluuk28@users.noreply.github.com>
2024-01-29 10:14:06 +0100

    Create VirtualMachineSetup2.ps1

---
+++ VirtualMachineSetup2.ps1
@@ -0,0 +1,14 @@
+param (
+  [Parameter(Mandatory=$false)]
+  [string]$IpAddress,
+  [Parameter(Mandatory=$false)]
+  [string]$Username,
+  [Parameter(Mandatory=$true)]
+  [string]$DomainName,
+  [Parameter(Mandatory=$true)]
+  [string]$Password
+)
+
+powershell -ExecutionPolicy bypass -File office-proplus-deployment.ps1 -OfficeVersion Office2016
+powershell -ExecutionPolicy bypass -File 7ZipSetup.ps1
+powershell -ExecutionPolicy bypass -File join-AD.ps1 -IpAddress $IpAddress -Username $Username -DomainName $DomainName -Password $Password
